c357e12f25183dfd22f3cb71857f78894672908b
AziretGT <esketcum430@gmail.com>
2023-03-04 15:08:20 +0600

    homework4

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path 
-from posts.views import hello, get_index, get_about, get_contact
+from django.urls import path
+from posts.views import hello, get_index, get_about, get_contacts, get_post, update_post, delete_post
+
 
 urlpatterns = [
-path ("hello/", hello, name ="hello-view"),
-path ("", get_index, name ="index-page"),
-path ("about/",get_about, name ="endpoint-about"),
-path ("contact/", get_contact, name="get-contact"),
-]     
+    path("hello/", hello, name = "hello-view"),    
+    path("", get_index, name = "index-page"),
+    path("conacts/", get_contacts, name = "contacts-view"),    
+    path("about/", get_about, name = "about-view"),    
+    path("post_creat/", get_post, name = "post-creat"),    
+    path("update_post/", update_post, name = "update-post"),    
+    path("delete_post/", delete_post, name = "delete_post"),    
+]
--- posts/views.py
+++ posts/views.py
@@ -1,49 +1,66 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from posts.models import Post
+
 
 def hello(request):
-    # mylist  = [1,2,3,4]
-    body = "<h1>Hello<h1>"
-    # body = """
-    # <!DOCTYPE html>
-    #     <html>
-    #     <head>
-    #         <title>Geek Test</title>
-    #     </head>
-    #     <body>
+    body = "<h1>Hello</h1>"
 
-    #         <h1>Зaголовок первого уровня</h1>
-    #         <p>Параграф</p>
+    headers = {"name":"Alex",
+                "Content-type":"application/vnd.ms-excel",
+                "Content-Disposition":"attachment; filename = file.xls"
+                }
+    return HttpResponse(body, headers = headers, status = 500)
 
-    #     </body> 
-    #     </html>
-    #  """
-    headers = {"names": "Alex"}
-
-# "Content-Type":"applivcation/vnd.ms-excel",
-# "Content-Dispasition": "attachment"} 
-    return HttpResponse( body,headers=headers,status=300)
 
 def get_index(request):
-    context = {
-        "title": "Main page",
-        "my_list": [1, 2, 3, 4],
-    }
-    return render(request, "posts/index.html", context=context)
-    # # print (request.__dict__)
+    posts  = Post.objects.filter(status=True)
+    # print(request.user)
     # if request.method == "GET":
     #     return HttpResponse("Главная страница")
-    # else :
-    #     return HttpResponse("Не тот метод запроса")
+    # else:
+    #     return HttpResponse("Не тот запрос")
+
+    context = {
+        "title": "Main page",
+        "posts": posts,
+    }
+    return render(request, "posts/index.html", context=context)
+
+
+def get_contacts(request):
+    context = {
+        "title": "Контакт"
+    }
+    return render(request, "posts/contacts.html", context=context)
+
+    # return HttpResponse("Контакты")
+
 
 def get_about(request):
-    context ={
-        "title" : "Страница о нас",
+    context = {
+        "title": "O нас"
     }
     return render(request, "posts/about.html", context=context)
 
-def get_contact(request):
-    context ={
-        "title" : "Контакты",
+
+def get_post(request):
+    context = {
+        "title": "Пост",
     }
-    return render(request, "posts/contact.html", context=context)
+    return render(request, "posts/post_create.html", context=context)
+
+
+def update_post(request):
+    context = {
+        "title": "Пост",
+    }
+    return render(request, "posts/post_update.html", context=context)
+
+
+def delete_post(request):
+    context = {
+        "title": "Пост",
+    }
+    return render(request, "posts/post_detail.html", context=context)
+# Create your views here.
